5da3842ddb454bf5f37ff9afef6df89188aabc3e
rosarubuser <xiaojieli6a@gmail.com>
2022-12-07 21:20:47 -0800

    update asset dao

---
+++ src/main/java/com/tcss559/asset/dao/AssetDAO.java
@@ -0,0 +1,25 @@
+package com.tcss559.asset.dao;
+
+import com.tcss559.asset.models.Asset;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AssetDAO {
+    public Asset selectByRFIDid(String RFIDid);
+
+    public List<Asset> selectByCategory(String category);
+
+    public List<Asset> selectByName(String name);
+
+    public List<Asset> selectByCity(String city);
+
+    public List<Asset> selectByCountry(String country);
+
+    public int insert(Asset asset);
+
+    public int update(Asset asset);
+
+    public int delete(int RFIDid);
+}
